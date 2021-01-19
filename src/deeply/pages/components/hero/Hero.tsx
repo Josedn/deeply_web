cc42693a57db567f87a599987f090b14f0e0fcba
Jose D <joserafael161@hotmail.com>
2021-01-19 16:33:37 -0500

    change gradient

--- src/deeply/pages/components/hero/Hero.tsx
+++ src/deeply/pages/components/hero/Hero.tsx
@@ -4,10 +4,21 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="hero__container">
-                deep linking made easy
+                <div className="hero__column hero__column--left">
+                    <h2 className="hero__title">
+                        Payments infrastructure for the internet
+                    </h2>
+                    <div className="hero__description">
+                        Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.
+                    </div>
+                </div>
+
+                <div className="hero__column hero__column--right">
+
+                </div>
             </div>
         </section>
     );
-}; 
+};
 
 export default Hero;
