b50ebb68bbe1f6bea0a5a1fc72e27bd2d883c929
Jose D <joserafael161@hotmail.com>
2021-01-19 20:38:11 -0500

    Add stripe gradient

--- src/deeply/pages/components/hero/Hero.tsx
+++ src/deeply/pages/components/hero/Hero.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Hero.scss';
 import Phone from './phone/Phone';
 import screenshot from '../../../images/instagram1.png';
+import Gradient from '../../../styles/stripe.gradient';
 
 const Hero = () => {
+    useEffect(() => {
+        const gradient = new Gradient();
+        gradient.initGradient(".gradient__canvas");
+        console.log(gradient);
+    }, []);
+
     return (
         <section className="hero">
             <div className="hero__container">
                 <div className="hero__column hero__column--left">
+                    <div className="hero__gradient">
+                        <canvas className="gradient__canvas"></canvas>
+                    </div>
                     <h2 className="hero__title">
                         Deep linking
                         <br></br>
@@ -16,6 +26,11 @@ const Hero = () => {
                     <div className="hero__description">
                         Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.
                     </div>
+                    <footer className="hero__footer">
+                        <button className="hero__button">
+                            <div>Start now</div><i className="hero__button-icon fas fa-chevron-right"></i>
+                        </button>
+                    </footer>
                 </div>
 
                 <div className="hero__column hero__column--right">
---
+++ src/deeply/styles/stripe.gradient.d.ts
@@ -0,0 +1,9 @@
+export default class Gradient {
+    constructor();
+    initGradient(selector: string);
+    toggleColor(index: number);
+    updateFrequency(freq: number);
+    pause();
+    play();
+    connect();
+}
