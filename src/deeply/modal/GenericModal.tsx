a9b4523f6db5d0ee3df2a9f966c225df1e47f201
Jose D <joserafael161@hotmail.com>
2021-03-07 23:28:49 -0500

    Working on modal

---
+++ src/deeply/modal/GenericModal.tsx
@@ -0,0 +1,13 @@
+import './GenericModal.scss';
+
+const GenericModal: React.FC = (props) => {
+    //const { children } = props;
+
+    return (
+        <div className="modal">
+            Hello world from modal
+        </div>
+    );
+};
+
+export default GenericModal;
--- src/deeply/pages/home/Home.tsx
+++ src/deeply/pages/home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import GenericModal from '../../modal/GenericModal';
 import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import Hero from '../components/hero/Hero';
@@ -8,6 +9,7 @@ import './Home.scss';
 const Home = () => {
     return (
         <>
+            <GenericModal />
             <Header />
             <Hero />
             <Services />
