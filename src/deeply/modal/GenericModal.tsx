d2307df63e7d4788bc844993e5819fd6f8fde56e
Jose D <joserafael161@hotmail.com>
2021-03-25 17:36:38 -0500

    Add ugly modal columns

--- src/deeply/modal/GenericModal.tsx
+++ src/deeply/modal/GenericModal.tsx
@@ -23,6 +23,28 @@ const ModalContent: React.FC = (props) => {
                     X
                 </button>
             </header>
+            <section className="modal__content">
+                <div className="app-list__column-left">
+                    <h4 className="app-list__title">
+                        Popular Apps
+                    </h4>
+                    <br className="app-list__separator" />
+                    <ul className="app-list__list">
+                        <li className="app-list__list-item">
+                            Instagram
+                        </li>
+                        <li className="app-list__list-item">
+                            Facebook
+                        </li>
+                    </ul>
+                </div>
+                <div className="app-list__column-right">
+                    <h3 className="app-content__title">
+                        Instagram
+                    </h3>
+                </div>
+            </section>
+
         </div>
     );
 };
