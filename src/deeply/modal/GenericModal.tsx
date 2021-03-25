9155203fb2bd6725d27c9a0c6668701c76e3fd0f
Jose D <joserafaeldn@gmail.com>
2021-03-25 18:43:29 -0500

    Add basic modal styles

--- src/deeply/modal/GenericModal.tsx
+++ src/deeply/modal/GenericModal.tsx
@@ -20,15 +20,14 @@ const ModalContent: React.FC = (props) => {
                     Hello world from modal
                 </h2>
                 <button className="modal__close">
-                    X
+                    <i className="modal__close-icon fas fa-times"></i>
                 </button>
             </header>
             <section className="modal__content">
                 <div className="app-list__column-left">
                     <h4 className="app-list__title">
-                        Popular Apps
+                        Categories
                     </h4>
-                    <br className="app-list__separator" />
                     <ul className="app-list__list">
                         <li className="app-list__list-item">
                             Instagram
