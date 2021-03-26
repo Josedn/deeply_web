7ed1f83f3f56ffee13f439e37ebbfc1a4441f3e2
Jose D <joserafaeldn@gmail.com>
2021-03-25 22:04:35 -0500

    Add supported apps preview

--- src/deeply/modal/GenericModal.tsx
+++ src/deeply/modal/GenericModal.tsx
@@ -1,4 +1,5 @@
 import './GenericModal.scss';
+import instagram from '../images/icons/instagram.svg';
 
 const GenericModal: React.FC = (props) => {
     //const { children } = props;
@@ -17,7 +18,7 @@ const ModalContent: React.FC = (props) => {
         <div className="modal__container">
             <header className="modal__header">
                 <h2 className="modal__title">
-                    Hello world from modal
+                    Supported Apps
                 </h2>
                 <button className="modal__close">
                     <i className="modal__close-icon fas fa-times"></i>
@@ -26,17 +27,17 @@ const ModalContent: React.FC = (props) => {
             <section className="modal__content">
                 <div className="app-list__column-left">
                     <h4 className="app-list__title">
-                        Categories
+                        Social media
                     </h4>
                     <ul className="app-list__list">
                         <li className="app-list__list-item">
                             <button className="app-list__list-button">
-                                Instagram
+                                Facebook
                             </button>
                         </li>
                         <li className="app-list__list-item">
                             <button className="app-list__list-button app-list__list-button--selected">
-                                Facebook
+                                Instagram
                             </button>
                         </li>
                         <li className="app-list__list-item">
@@ -50,11 +51,57 @@ const ModalContent: React.FC = (props) => {
                             </button>
                         </li>
                     </ul>
+                    <h4 className="app-list__title">
+                        Custom
+                    </h4>
+                    <ul className="app-list__list">
+                        <li className="app-list__list-item">
+                            <button className="app-list__list-button">
+                                Custom deep link
+                            </button>
+                        </li>
+                    </ul>
                 </div>
                 <div className="app-list__column-right">
-                    <h3 className="app-content__title">
-                        Instagram
-                    </h3>
+                    {/*<div className="app-content__go-back-container">
+                        <i className="app-content__go-back-icon fas fa-arrow-left"></i> Back to apps
+                    </div>*/}
+                    <header className="app-content__header">
+                        <img className="app-content__image" src={instagram} alt="test"></img>
+                        <h3 className="app-content__title">
+                            Instagram
+                        </h3>
+                    </header>
+                    <section className="app-content__subtitle">
+                        <strong>We currently support the following types</strong>
+                        <br />
+                        Select a type to see more details
+                    </section>
+                    <section className="app-content__options">
+                        <ul className="app-content__type-list">
+                            <li className="app-content__type-item">
+                                <button className="app-content__type-button">
+                                    Home
+                                </button>
+                            </li>
+                            <li className="app-content__type-item">
+                                <button className="app-content__type-button app-content__type-button--selected">
+                                    Profile
+                                </button>
+                            </li>
+                            <li className="app-content__type-item">
+                                <button className="app-content__type-button">
+                                    Tag
+                                </button>
+                            </li>
+                        </ul>
+                    </section>
+                    <section className="app-content__preview">
+                        <div className="app-content__preview-box">
+                            <div className="app-content__preview-title">URL format</div>
+                            <div className="app-content__preview-content">instagram.com/<span className="app-content__preview-highlight">&lt;profile&gt;</span></div>
+                        </div>
+                    </section>
                 </div>
             </section>
 
